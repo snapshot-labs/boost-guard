a91ba597ea7435a0bd7bbaea32e7de49ae536fc4
Scott Piriou <30843220+pscott@users.noreply.github.com>
2023-12-13 11:37:33 +0100

    feat: validate end time

--- src/create_voucher.rs
+++ src/create_voucher.rs
@@ -4,6 +4,7 @@ use axum::response::IntoResponse;
 use graphql_client::{GraphQLQuery, Response as GraphQLResponse};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use std::time::SystemTime;
 
 // TODO: check with BIG voting power (f64 precision?)
 
@@ -54,20 +55,19 @@ pub async fn create_voucher_handler(
 
     let client = reqwest::Client::new();
     for request in requests {
-        println!("{request:?}");
-        let (proposal_type, proposal_score) =
-            get_proposal_info(client.clone(), &request.proposal_id).await?;
+        let proposal = get_proposal_info(client.clone(), &request.proposal_id).await?;
         let (voting_power, choice) =
             get_voter_info(client.clone(), &request.voter_address, &request.proposal_id).await?;
 
         let cap = None; // TODO: get this from ... somewhere?
         let boosted_choice = BoostStrategy::Incentive; // TODO: get this from ... somewhere?
-        let boost_pool = 100_f64;
+        let boost_pool = 100_f64; // TODO: ge tthis from... somewhere?
 
-        // ensure proposal has ended
-        validate_type(&proposal_type)?;
+        validate_end_time(proposal.end)?;
+        validate_type(&proposal.type_)?;
         validate_choice(choice, boosted_choice)?;
-        let _reward = compute_user_reward(boost_pool, voting_power, proposal_score, cap);
+
+        let _reward = compute_user_reward(boost_pool, voting_power, proposal.score, cap);
 
         // TODO: check cap
     }
@@ -92,10 +92,24 @@ fn compute_user_reward(
     }
 }
 
-fn validate_type(proposal_type: &str) -> Result<(), ServerError> {
-    if (proposal_type != "single-choice") && (proposal_type != "basic") {
+fn validate_end_time(end: u64) -> Result<(), ServerError> {
+    let current_timestamp = SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    if current_timestamp < end {
         Err(ServerError::ErrorString(format!(
-            "`{proposal_type:}` proposals are not eligible for boosting"
+            "proposal has not ended yet: {end:} > {current_timestamp:}",
+        )))
+    } else {
+        Ok(())
+    }
+}
+
+fn validate_type(type_: &str) -> Result<(), ServerError> {
+    if (type_ != "single-choice") && (type_ != "basic") {
+        Err(ServerError::ErrorString(format!(
+            "`{type_:}` proposals are not eligible for boosting"
         )))
     } else {
         Ok(())
@@ -151,10 +165,35 @@ async fn get_voter_info(
     Ok((vote.vp.ok_or("missing vp from the hub")?, vote.choice))
 }
 
+#[derive(Debug)]
+struct Proposal {
+    type_: String,
+    score: f64,
+    end: u64,
+}
+
+impl TryFrom<proposal_query::ProposalQueryProposal> for Proposal {
+    type Error = ServerError;
+
+    fn try_from(proposal: proposal_query::ProposalQueryProposal) -> Result<Self, Self::Error> {
+        let proposal_type = proposal.type_.ok_or("missing proposal type from the hub")?;
+        let proposal_score = proposal
+            .scores_total
+            .ok_or("missing proposal score from the hub")?;
+        let proposal_end = proposal.end.try_into()?;
+
+        Ok(Proposal {
+            type_: proposal_type,
+            score: proposal_score,
+            end: proposal_end,
+        })
+    }
+}
+
 async fn get_proposal_info(
     client: reqwest::Client,
     proposal_id: &str,
-) -> Result<(String, f64), ServerError> {
+) -> Result<Proposal, ServerError> {
     let variables = proposal_query::Variables {
         id: proposal_id.to_owned(),
     };
@@ -163,16 +202,10 @@ async fn get_proposal_info(
 
     let res = client.post(HUB_URL).json(&request_body).send().await?;
     let response_body: GraphQLResponse<proposal_query::ResponseData> = res.json().await?;
-    let proposal: proposal_query::ProposalQueryProposal = response_body
+    let proposal_query: proposal_query::ProposalQueryProposal = response_body
         .data
         .ok_or("missing data from the hub")?
         .proposal
         .ok_or("missing proposal data from the hub")?;
-
-    let proposal_type = proposal.type_.ok_or("missing proposal type from the hub")?;
-    let proposal_score = proposal
-        .scores_total
-        .ok_or("missing proposal score from the hub")?;
-
-    Ok((proposal_type, proposal_score))
+    Proposal::try_from(proposal_query)
 }
