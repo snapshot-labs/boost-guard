4898556847afcb8eefd748a803fc65d336e05818
Scott Piriou <30843220+pscott@users.noreply.github.com>
2024-02-14 13:34:11 +0100

    add testnet feature

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,11 @@ lazy_static! {
     static ref HUB_URL: String = {
         dotenv().ok();
 
-        #[cfg(test)]
+        #[cfg(feature = "testnet")]
         return env::var("TESTNET_HUB_URL")
             .expect("Please add TESTNET_HUB_URL to your environment or .env file.");
 
-        #[cfg(not(test))]
+        #[cfg(not(feature = "testnet"))]
         return env::var("HUB_URL").expect("Please add HUB_URL to your environment or .env file.");
     };
     static ref SUBGRAPH_URLS: HashMap<&'static str, String> = {
